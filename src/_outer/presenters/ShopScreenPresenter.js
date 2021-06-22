eca2305e08d317dd455be4699ed61e090fb0125a
paulmboyce <paul.boyce@goobito.com>
2021-06-22 14:05:50 +0100

    refactor(add views/web placeholder): so we can "be conscient" of a potential Web react app
    This helps avoid pushing code that would be shared up into the native app

--- src/_outer/presenters/ShopScreenPresenter.js
+++ src/_outer/presenters/ShopScreenPresenter.js
@@ -3,6 +3,26 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { getProductsAction } from "../../_middle/controllers";
 
+/**
+ * NOTE: ShopScreenPresenter is a React component, and uses
+ * useSelector() hook so is wired to our redux State.
+ * This gives standard React refresh on state changes.
+ *
+ * Because it is a React Component it lives in _outer.
+ *
+ * Our Presenter additionally passes in the Controller(Actions).
+ * This could be separaed, but here seems a fair location,
+ * allowing our actual ScreenComponents to receive all data and
+ * Controller/actions as params and hence be easy to isolate for Testing.
+ *
+ * BUT.. note that ShopScreenPresenter is NOT dependent on
+ * the _outer/views/mobile/ShopScreen component and any
+ * component uing ame props could be passed in.
+ * For example, it could equally be provided a React/web/component.
+ *
+ * This keeps code dependencies directed inward,
+ * aligned for Clean Architecture.
+ */
 const ShopScreenPresenter = ({ navigation, screenComponent: ShopScreen }) => {
 	const dispatch = useDispatch();
 
---
+++ src/_outer/views/web/README.txt
@@ -0,0 +1,12 @@
+This is where we would put our React Web components.
+
+We could then reuse ALL our:
+- Redux, 
+- Actions, 
+- Controllers, 
+- Presenters, etc.
+
+Providing a Web App would be minimal coding.
+
+We would simply rewrite the native JSX/styles of 
+the equivalent native components into web JSX (~HTML/css).
